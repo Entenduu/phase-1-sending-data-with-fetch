6795c514f9b8620a8a11272b7879898aa8becbf4
Cristian <Entenduu@gmail.com>
2022-06-13 17:53:52 -0400

    commit tree 8940c39f0bd1c4b74c07480508ced286b1f86402

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+function submitData(name,email) {
+    const formData = {
+        name: name,
+        email: email
+    }
+
+    const configurationObject = {
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json',
+            'Accept': "application/json",
+        },
+        body: JSON.stringify(formData)
+    }
+
+    const element = document.getElementsByTagName('body');
+
+
+
+    return fetch('http://localhost:3000/users', 
+    configurationObject)
+    .then((res) => res.json())
+    .then((obj) => document.body.innerHTML = obj.id)
+    .catch((error) => {
+        alert('Unauthorized Access');
+        document.body.innerHTML = error.message;
+        
+    })
+}
